33ef0ae38e165cadd16e122f0f85214d650e58fd
gulsumyalcn <120015953+gulsumyalcn@users.noreply.github.com>
2023-09-24 23:21:35 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace uygulama3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
